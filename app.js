cd01ed5a51d203035dd82169cb49ff7f7000a5ca
Marcelo <theg119.g@gmail.com>
2024-08-23 13:57:39 -0600

    Prueba de copiar al portapales

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+function cambiarTextoASalida(){
+    let valorEntrada = document.getElementById('1');
+    let valorSalida = document.getElementById('2');
+    valorSalida.value = valorEntrada.value;
+}
+
+
+
+    let texto = document.getElementById('2').innerHTML;
+  const copiarTexto = async () => {
+    try {
+      await navigator.clipboard.writeText(texto);
+      console.log('Contenido copiado al portapapeles');
+    } catch (err) {
+      console.error('Error al copiar: ', err);
+    }
+  }
+
