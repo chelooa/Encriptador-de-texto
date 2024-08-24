3a60895e3dd0497a86280d074fbeca85ea64b3c4
Marcelo <theg119.g@gmail.com>
2024-08-23 22:18:38 -0600

    Funcionalidad de los botones

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 
-function cambiarTextoASalida(){
+function cambiarTextoASalida(texto){
     let valorEntrada = document.getElementById('1');
     let valorSalida = document.getElementById('2');
-    valorSalida.value = valorEntrada.value;
+    valorSalida.value = texto;
 }
 
 
@@ -18,3 +18,50 @@ const copiarTexto = async () => {
   }
 }
 
+/*La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+Debe funcionar solo con letras minúsculas
+No deben ser utilizados letras con acentos ni caracteres especiales
+Debe ser posible convertir una palabra para la versión encriptada 
+también devolver una palabra encriptada para su versión original.*/
+
+function encriptar() {
+  let texto = document.getElementById('1').value;
+
+  // Expresión regular con alternativas para 'a' y 'u'
+  const expresionRegular = /(a|u|i|o|e)/g;
+  
+  // Función de reemplazo
+  let nuevoTexto = texto.replace(expresionRegular, match => {
+    if (match === 'a') return 'ai';
+    if (match === 'u') return 'ufat';
+    if (match === 'i') return 'imes';
+    if (match === 'o') return 'ober';
+    if (match === 'e') return 'enter';
+    return match; // Por si acaso no coincide con 'a' ni con 'u'
+  });
+
+  cambiarTextoASalida(nuevoTexto);
+}
+
+function desencriptar() {
+  let texto = document.getElementById('1').value;
+
+  // Expresión regular con alternativas para 'a' y 'u'
+  const expresionRegular = /(ai|ufat|imes|ober|enter)/g;
+  
+  // Función de reemplazo
+  let nuevoTexto = texto.replace(expresionRegular, match => {
+    if (match === 'ai') return 'a';
+    if (match === 'ufat') return 'u';
+    if (match === 'imes') return 'i';
+    if (match === 'ober') return 'o';
+    if (match === 'enter') return 'e';
+    return match; // Por si acaso no coincide con 'a' ni con 'u'
+  });
+
+  cambiarTextoASalida(nuevoTexto);
+}
